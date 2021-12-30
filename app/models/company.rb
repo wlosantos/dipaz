1ac77ad76e9ccbe600f109c8f379a250210e3cda
Wendel Lopes <wendelopes@msn.com>
2021-12-30 12:07:00 -0300

    created company model with test and validations

---
+++ app/models/company.rb
@@ -0,0 +1,39 @@
+class Company < ApplicationRecord
+  enum status: { active: 0, blocked: 1 }
+
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :cnpj, presence: true,
+                   uniqueness: { message: 'already registered for a company!' },
+                   length: { minimum: 18 }
+
+  before_validation :allow_create?, on: :create
+  before_validation :allow_update?, on: :update
+
+  private
+
+  def cnpj_valid?
+    errors.add(:cnpj, 'is not valid!') unless CNPJ.valid?(cnpj)
+    CNPJ.valid?(cnpj)
+  end
+
+  def cnpj_not_exists?
+    return true if Company.where(cnpj: cnpj).first.nil?
+
+    errors.add(:cnpj, 'already registered!')
+    false
+  end
+
+  def cnpj_approved?
+    cnpj_valid? && !cnpj_not_exists?
+  end
+
+  def allow_create?
+    cnpj_approved?
+  end
+
+  def allow_update?
+    return false if cnpj_changed? && !cnpj_approved?
+
+    true
+  end
+end
