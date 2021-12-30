d0a93b8c778c0a5b7d781c28b82a31ef83b6c66c
Wendel Lopes <wendelopes@msn.com>
2021-12-30 20:06:22 -0300

    created #update action company with tests

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     # Actions companies
     class CompaniesController < ApplicationController
+      before_action :set_company, only: %i[update]
       def index
         company = Company.first
         if company.active?
@@ -9,6 +10,26 @@ module Api
         else
           render json: { warning: 'Company blocked. Contact support!' }, status: :unauthorized
         end
+      end
+
+      def update
+        if @company.blocked?
+          render json: { warning: 'Company blocked. Contact support!' }, status: :unauthorized
+        elsif @company.update(params_company)
+          render json: @company, status: :ok
+        else
+          render json: { errors: @company.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_company
+        @company = Company.find(params[:id])
+      end
+
+      def params_company
+        params.require(:company).permit(:name, :cnpj, :domain)
       end
     end
   end
--- spec/requests/api/v1/companies_spec.rb
+++ spec/requests/api/v1/companies_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Companies', :focus, type: :request do
   let!(:company) { create(:company, status: set_status) }
+  let(:headers) do
+    {
+      'Accept': 'application/vnd.dipaz.v1',
+      'Content-Type': Mime[:json].to_s
+    }
+  end
+
   describe 'GET /companies' do
     before do
       company
-      get '/companies', params: {}, headers: { 'Content-Type' => 'application/json' }
+      get '/companies', params: {}, headers: headers
     end
 
     context 'returns successfully' do
@@ -17,9 +24,39 @@ RSpec.describe 'Companies', :focus, type: :request do
     context 'access not allowed' do
       let(:set_status) { 'blocked' }
       before do
-        get '/companies', params: {}, headers: { 'Content-Type' => 'application/json' }
+        get '/companies', params: {}, headers: headers
       end
 
+      it { expect(response).to have_http_status :unauthorized }
+      it { expect(json_body[:warning]).to eq('Company blocked. Contact support!') }
+    end
+  end
+
+  describe 'PUT /companies/:id' do
+    before do
+      company
+      put "/companies/#{company.id}", params: params_company.to_json, headers: headers
+    end
+
+    context 'updated successful' do
+      let(:set_status) { 'active' }
+      let(:params_company) { attributes_for(:company, name: 'W3n Designer') }
+
+      it { expect(response).to have_http_status :success }
+      it { expect(json_body[:name]).to eq(params_company[:name]) }
+    end
+
+    context 'updated failure' do
+      let(:set_status) { 'active' }
+      let(:params_company) { attributes_for(:company, name: '') }
+
+      it { expect(response).to have_http_status :unprocessable_entity }
+      it { expect(json_body[:errors]).to include("Name can't be blank") }
+    end
+
+    context 'updated company blocked' do
+      let(:set_status) { 'blocked' }
+      let(:params_company) { attributes_for(:company, name: 'W3n Designer') }
       it { expect(response).to have_http_status :unauthorized }
       it { expect(json_body[:warning]).to eq('Company blocked. Contact support!') }
     end
