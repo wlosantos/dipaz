781cc05f8b2e052697d9c713a300034d1d5e92b4
Wendel Lopes <wendelopes@msn.com>
2021-12-31 17:32:58 -0300

    created #show action register with tests

--- app/controllers/api/v1/registers_controller.rb
+++ app/controllers/api/v1/registers_controller.rb
@@ -2,10 +2,15 @@ module Api
   module V1
     class RegistersController < ApplicationController
       before_action :set_company, only: %i[index]
+      before_action :set_register, only: %i[show]
 
       def index
         register = @company.registers.all
         render json: register, status: :ok
+      end
+
+      def show
+        render json: @register, status: :ok
       end
 
       private
@@ -14,6 +19,12 @@ module Api
         @company = current_user.company
       rescue StandardError
         head :unauthorized
+      end
+
+      def set_register
+        @register = Register.find(params[:id])
+      rescue StandardError
+        head :unprocessable_entity
       end
     end
   end
--- spec/requests/api/v1/registers_spec.rb
+++ spec/requests/api/v1/registers_spec.rb
@@ -37,4 +37,28 @@ RSpec.describe 'Api::V1::Registers', :focus, type: :request do
       it { expect(response).to have_http_status :unauthorized }
     end
   end
+
+  describe 'GET /companies/:id/registers/:id' do
+    let(:register) { create(:register, company: company) }
+
+    context 'returns successfully' do
+      before do
+        register
+        get "/companies/#{company.id}/registers/#{register.id}", params: {}, headers: headers
+      end
+
+      it { expect(response).to have_http_status :success }
+      it 'return json data of register' do
+        expect(json_body[:cpf]).to eq(register.cpf)
+      end
+    end
+
+    context 'failures' do
+      before do
+        get "/companies/#{company.id}/registers/1000", params: {}, headers: headers
+      end
+
+      it { expect(response).to have_http_status :unprocessable_entity }
+    end
+  end
 end
