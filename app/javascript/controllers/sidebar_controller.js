670748e35179b483c23e3a99be03d475321145f7
wendellopes <wendelopes@msn.com>
2023-02-16 11:44:01 -0300

    create mobile efect to sidebar

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -11,4 +11,15 @@ export default class extends Controller {
     const submenu = event.target.nextElementSibling
     submenu.classList.toggle("hidden")
   }
+
+  showSidebar(e){
+    e.preventDefault()
+    const sidebar = document.getElementById("sidebar")
+    const btn_sidebar = document.getElementById("show-sidebar")
+    const bk_menu = document.getElementById("bk-menu")
+
+    sidebar.classList.toggle("left-[-300px]")
+    btn_sidebar.classList.toggle("hidden")
+    bk_menu.classList.toggle("hidden")
+  }
 }
